f76f97b6627722f4ca2e654ccda6e44d8f786976
Siddeequa Jumeen <65298005+Siddeequa@users.noreply.github.com>
2021-07-05 02:32:28 +0800

    Update RangeGenerator.py

--- Codewars/Python/Level 8/RangeGenerator.py
+++ Codewars/Python/Level 8/RangeGenerator.py
@@ -1,4 +1,5 @@
-# Implement a function named generateRange(min, max, step), which takes three arguments and generates a range of integers from min to max, with the step. The first integer is the minimum value, the second is the maximum of the range and the third is the step. (min < max)
+# Implement a function named generateRange(min, max, step), which takes three arguments and generates a range of integers from min to max, with the step. 
+# The first integer is the minimum value, the second is the maximum of the range and the third is the step. (min < max)
 
 def generate_range(min, max, step):
     # Initialise list
